5716b59a6ac9ca95ca67ef1cdd27cf882b886476
qualidadehsr-rgb <qualidade.hsr@gruposanta.com.br>
2026-07-01 13:55:09 -0400

    feat: adiciona dim_competencia

---
+++ dbt/pipeline_analytics/models/marts/dim_competencia.sql
@@ -0,0 +1,28 @@
+-- tabela para gerar competência dinamicamente a partir dos dados existentes
+with competencias as(
+    select distinct competencia
+    from {{ ref('atendimentos_pa') }}
+),
+
+final as(
+    select
+        competencia,
+        extract(year from competencia) as ano,
+        extract(month from competencia) as mes,
+        case
+            when extract(month from competencia) = 1 then 'Jan'
+            when extract(month from competencia) = 2 then 'Fev'
+            when extract(month from competencia) = 3 then 'Mar'
+            when extract(month from competencia) = 4 then 'Abr'
+            when extract(month from competencia) = 5 then 'Mai'
+            when extract(month from competencia) = 6 then 'Jun'
+            when extract(month from competencia) = 7 then 'Jul'
+            when extract(month from competencia) = 8 then 'Ago'
+            when extract(month from competencia) = 9 then 'Set'
+            when extract(month from competencia) = 10 then 'Out'
+            when extract(month from competencia) = 11 then 'Nov'
+            when extract(month from competencia) = 12 then 'Dez' end as nome_mes,
+        extract(quarter from competencia) as trimestre
+    from competencias
+)
+select * from final
